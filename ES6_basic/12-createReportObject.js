5e9d02bb0125e570ebd379bd42afb1ea41d0bdd7
Allison <allisonabinger@gmail.com>
2024-01-04 15:46:58 -0600

    A - 01/04 03:46 PM - 12 tested

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+export default function createReportObject(employeesList) {
+const createReportObject = {
+  allEmployees: {
+    ...employeesList,
+  },
+  getNumberOfDepartments() {
+    return Object.keys(this.allEmployees).length;
+  },
+};
+return createReportObject;
+}
