320f642077f6d70202f94127dc6df088b4811d00
Allison <allisonabinger@gmail.com>
2024-01-04 16:18:21 -0600

    A - 01/04 04:18 PM - linter fixes, removed .js from import, wont test on local but passes linter in checker

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms(){
+export default function initializeRooms() {
   const sizes = [19, 20, 34];
   const classrooms = sizes.map((size) => new ClassRoom(size));
   return classrooms;
