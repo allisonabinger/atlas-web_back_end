46dd8c061ff127fcc64a822a6ae98feabd13b76d
Allison <allisonabinger@gmail.com>
2024-01-04 16:15:05 -0600

    A - 01/04 04:15 PM - 1 tested

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+import ClassRoom from "./0-classroom";
+
+export default function initializeRooms {
+  sizes = [19, 20, 34];
+  const classrooms = sizes.map((size) => new ClassRoom(size));
+  return classrooms;
+}
