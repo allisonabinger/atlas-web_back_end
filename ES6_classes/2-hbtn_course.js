135de126e3268d731d25c6d9daeeccfa9deb59cc
Allison <allisonabinger@gmail.com>
2024-01-05 12:42:10 -0600

    a - 01/05 12:42 PM - testing 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,47 @@
 export default class HolbertonCourse {
+  /* constructor - called upon object creation */
   constructor(name, length, students) {
-    
-  };
-}
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string.');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number.');
+    }
+    if (typeof students !== 'object') {
+      throw new TypeError('Students must be an array object.');
+      /* not sure if this should be array or object?? */
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  /* getters */
+  get name() {
+    return this._name;
+  }
+  get length() {
+    return this._length;
+  }
+  get students() {
+    return this._students;
+  }
+  /* setters */
+  set name(updatedname) {
+    if (typeof updatedname !== 'string') {
+      throw new TypeError('New name must be a string');
+    }
+    this._name = updatedname;
+    }
+  set length(updatedlength) {
+    if (typeof updatedlength !== 'number') {
+      throw new TypeError('New length must be a number');
+    }
+    this._name = updatedlength;
+    }
+  set students(updatedstudents) {
+    if (typeof updatedstudents !== 'object') {
+      throw new TypeError('New students must be an array object');
+    }
+    this._name = updatedstudents;
+    }
+  }
