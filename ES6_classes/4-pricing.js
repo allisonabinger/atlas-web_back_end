67c3f6625ae9efcb5f832043b5837e9ad442ecc0
Allison <allisonabinger@gmail.com>
2024-01-05 13:42:10 -0600

    A - 01/05 01:41 PM - passes lint, removed .js from import, won't test in local without it

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,13 +1,14 @@
-import Currency from './3-currency.js';
+/* eslint-disable no-underscore-dangle */
+import Currency from './3-currency';
 /* although error catching is important, this class does not have 100% error
 catching capabilities */
 
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
-    this._currency = currency
+    this._currency = currency;
   }
-  
+
   /* getters */
 
   get amount() {
@@ -20,7 +21,7 @@ export default class Pricing {
 
   /* setters */
   set amount(newAmt) {
-    if (typeof new newAmt === 'number') {
+    if (typeof newAmt === 'number') {
       this._amount = newAmt;
     } else {
       throw new TypeError('New amount must be a number');
@@ -39,7 +40,8 @@ export default class Pricing {
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
+
   static convertPrice(amount, conversionRate) {
-    return amount * conversionRate
+    return amount * conversionRate;
   }
 }
