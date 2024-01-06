eceab009a1f3118bfbe6fe75348d780d4f7303a7
Allison <allisonabinger@gmail.com>
2024-01-05 23:01:19 -0600

    A - 01/05 11:01 PM - skyhigh implemented, testing now

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  /* getters */
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuation slowly the ${this._floors} floors.`;
+  }
+}
