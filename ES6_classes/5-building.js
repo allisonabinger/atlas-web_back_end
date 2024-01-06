9c8ae67c59f5784429420ddd2413b4aea59a71b0
Allison <allisonabinger@gmail.com>
2024-01-05 22:57:18 -0600

    A - 01/05 10:57 PM - removed typo in message

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (!this.evacuationWarningMessage && this.constructor !== Building) {
-      throw new TypeError('Class extending Building must override evacuationWarningMessage.');
+      throw new TypeError('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
