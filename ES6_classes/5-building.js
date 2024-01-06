de8d664fbc38d10dade89ad849992fde709ed88c
Allison <allisonabinger@gmail.com>
2024-01-05 22:56:44 -0600

    A - 01/05 10:56 PM - lint passes, evac method passes

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,13 @@
 export default class Building {
   constructor(sqft) {
+    if (!this.evacuationWarningMessage && this.constructor !== Building) {
+      throw new TypeError('Class extending Building must override evacuationWarningMessage.');
+    }
     this._sqft = sqft;
   }
 
   /* getters */
   get sqft() {
     return this._sqft;
-  }
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage')
   }
 }
