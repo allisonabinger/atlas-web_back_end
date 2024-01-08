3fdad81fb202cf09d2d9201da5a887dc6d290262
Allison <allisonabinger@gmail.com>
2024-01-08 14:03:21 -0600

    A - 01/08 02:03 PM - passes main test and lint

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 export default function getStudentsByLocation(studentList, city) {
   let filteredStudents = [];
-  filteredStudents = studentList.filter(student => student.location === city);
+  filteredStudents = studentList.filter((student) => student.location === city);
   return filteredStudents;
 }
