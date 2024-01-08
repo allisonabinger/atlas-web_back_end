94115c3ab8ef4a3eacb8e5dd394d40e9f33e035f
Allison <allisonabinger@gmail.com>
2024-01-08 14:27:41 -0600

    A - 01/08 02:27 PM - beginnings of 4, stepping away for a moment

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,5 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  const studentByLocation = studentList.filter((students) => studentList.location === city);
+  return studentByLocation.map(
+  )
+}
