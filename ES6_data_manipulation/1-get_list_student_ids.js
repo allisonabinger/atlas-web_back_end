dc075076e88795e0a60f94256876891567957d42
Allison <allisonabinger@gmail.com>
2024-01-08 13:38:30 -0600

    A - 01/08 01:38 PM - Passes eslint

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
-export default function getListStudentIds (studentarray) {
+export default function getListStudentIds(studentarray) {
   let ids = [];
   if (Array.isArray(studentarray)) {
-    ids = studentarray.map(student => student.id);
+    ids = studentarray.map((student) => student.id);
   }
   return ids;
 }
