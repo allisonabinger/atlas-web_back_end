6b494f1c3d31745a44494464098edbaf5479fab7
Allison <allisonabinger@gmail.com>
2024-01-08 16:15:02 -0600

    A - 01/08 04:14 PM - added join to return statement

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,8 +2,8 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
-  return Array.from(set)
+  const newstring = Array.from(set)
     .filter((item) => item.startsWith(startString))
-    .map((item) => item.slice(startString.length))
-    .join('-');
+    .map((item) => item.slice(startString.length));
+  return newstring.join('-');
 }
