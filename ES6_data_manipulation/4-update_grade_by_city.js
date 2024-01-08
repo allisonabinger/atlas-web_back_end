5cc1f57e9cd9996a635f3f8f906e52ba0ccaa643
Allison <allisonabinger@gmail.com>
2024-01-08 15:15:16 -0600

    A - 01/08 03:15 PM - lint passed

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
   /* use filter to create new array with students who live in specified city */
-  let studentsInCity = studentList.filter((student) => student.location === city);
+  const studentsInCity = studentList.filter((student) => student.location === city);
   /* use map to update grades based on newGrades array */
-  let updatedStudents = studentsInCity.map((student) => {
+  const updatedStudents = studentsInCity.map((student) => {
     /* search for newGrade by student id using find */
-    let newGrade = newGrades.find((ng) => ng.studentId === student.id);
+    const newGrade = newGrades.find((ng) => ng.studentId === student.id);
     /* return new student object with new grade if it exists */
-    return { ...student, grade: newGrade ? newGrade.grade : 'N/A'};
+    return { ...student, grade: newGrade ? newGrade.grade : 'N/A' };
   });
   return updatedStudents;
 }
