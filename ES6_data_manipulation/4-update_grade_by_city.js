cb8338c89a29dbdee85aefc0cce60f59291a6d61
Allison <allisonabinger@gmail.com>
2024-01-08 15:14:35 -0600

    A - 01/08 03:14 PM - 4 is main tested finally!!

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,12 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-  const studentByLocation = studentList.filter((students) => studentList.location === city);
-  return studentByLocation.map(
-  )
+  /* use filter to create new array with students who live in specified city */
+  let studentsInCity = studentList.filter((student) => student.location === city);
+  /* use map to update grades based on newGrades array */
+  let updatedStudents = studentsInCity.map((student) => {
+    /* search for newGrade by student id using find */
+    let newGrade = newGrades.find((ng) => ng.studentId === student.id);
+    /* return new student object with new grade if it exists */
+    return { ...student, grade: newGrade ? newGrade.grade : 'N/A'};
+  });
+  return updatedStudents;
 }
