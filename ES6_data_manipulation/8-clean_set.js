8397389154e414873198dd02b5a56e86fc559c25
Allison <allisonabinger@gmail.com>
2024-01-08 16:38:58 -0600

    a - 01/08 04:38 PM - added data type check to 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (startString === '' || typeof startString !== 'string') {
     return '';
   }
-  const newstring = Array.from(set)
+  const newstring  =  Array.from(set)
     .filter((item) => item.startsWith(startString))
     .map((item) => item.slice(startString.length));
   return newstring.join('-');
