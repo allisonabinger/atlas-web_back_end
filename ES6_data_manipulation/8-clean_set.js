1820093b8397a47d7ee6182074af21ac7e0af384
Allison <allisonabinger@gmail.com>
2024-01-08 16:11:56 -0600

    A - 01/08 04:11 PM - re-worked to use array instead of for..of

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,11 +2,8 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
-  let result = [];
-  for (let item of set) {
-    if(item.startsWith(startString)) {
-      result.push(item.slice(startString.length));
-    }
-  }
-  return result.join('-');
+  return Array.from(set)
+  .filter((item) => item.startsWith(startString))
+  .map((item) => item.slice(startString.length))
+  .join('-');
 }
