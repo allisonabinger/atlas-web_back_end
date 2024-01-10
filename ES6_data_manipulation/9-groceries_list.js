82fdf19ac0e4cb6a665e0c2a09aa2c532097c0f4
Allison <allisonabinger@gmail.com>
2024-01-10 11:23:11 -0600

    A - 01/10 11:23 AM - working on 9, added new Map, no return yet

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,17 @@
+export default function groceriesList() {
+  const glist = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ])
+  
+}
+
+
+Apples, 10
+Tomatoes, 10
+Pasta, 1
+Rice, 1
+Banana, 5
