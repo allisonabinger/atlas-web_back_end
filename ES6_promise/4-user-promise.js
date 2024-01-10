5b4223e91e7b52357e94aa509f6fa529ba775186
Allison <allisonabinger@gmail.com>
2024-01-10 14:34:04 -0600

    A - 01/10 02:34 PM - passes main, untested lint

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,7 @@
+// unsure if this one was designed to import from utils, not used here
+
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    resolve({ firstName, lastName})
+  })
+}
