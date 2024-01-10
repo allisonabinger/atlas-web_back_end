a76dd96b036448a3cd1214d393705d0feb7d2efc
Allison <allisonabinger@gmail.com>
2024-01-10 11:24:46 -0600

    A - 01/10 11:24 AM - main test passed, lint passed

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -4,14 +4,7 @@ export default function groceriesList() {
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
-    ['Banana', 5]
-  ])
-  
+    ['Banana', 5],
+  ]);
+  return glist;
 }
-
-
-Apples, 10
-Tomatoes, 10
-Pasta, 1
-Rice, 1
-Banana, 5
