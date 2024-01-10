ceff78fb2bae43b287adf4f68ba3bd2ece487b7d
Allison <allisonabinger@gmail.com>
2024-01-10 13:03:06 -0600

    A - 01/10 01:02 PM - high-order was too tricky, reverting to different method

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,18 +1,14 @@
 /* eslint-disable no-unused-vars */
 export default function handleResponseFromAPI(promise) {
-  // Prompt:
-  // when promise resolves, return response object
-  // when promise rejects, return an empty Error object
-  // log response to console for every resolution
-  const logAndReturn = (handler) => (...args) => {
-    // high-order, takes handler function and returns new function
-    // logs messages then does handler function
-    // i may not use this again because it was so tricky
-    console.log('Got a response from the API');
-    return handler(...args);
-  };
-  promise.then(
-    logAndReturn((value) => ({ status: 200, body: 'success' })),
-    logAndReturn((reason) => new Error()),
+  promise
+  .then(
+    (value) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    },
+    (reason) => {
+      console.log('Got a response from the API');
+      return new Error();
+    },
   );
 }
