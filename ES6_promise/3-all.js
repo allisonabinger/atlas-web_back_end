265e3eeff874239c6e9db8021eeba0052efaf45f
Allison <allisonabinger@gmail.com>
2024-01-10 14:08:32 -0600

    A - 01/10 02:08 PM - readded .js to resolve issues

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
