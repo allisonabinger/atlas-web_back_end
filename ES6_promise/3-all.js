e30d99cdfbc416b94d7456c414e668e72f3e5dc3
Allison <allisonabinger@gmail.com>
2024-01-10 14:06:56 -0600

    A - 01/10 02:06 PM - passes lint, extension removed from ./utils

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils.js'
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
-      console.log(`${body} ${user.firstName} ${user.lastName}`);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
-      console.log('Signup system offline')
-    })
+      console.log('Signup system offline');
+    });
 }
