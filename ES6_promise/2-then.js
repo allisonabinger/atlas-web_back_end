3ff03ff379efe67015f4cc13c8666417a06f89e6
Allison <allisonabinger@gmail.com>
2024-01-10 13:04:26 -0600

    A - 01/10 01:04 PM - passes lint! passed main, too!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 export default function handleResponseFromAPI(promise) {
   promise
-  .then(
-    (value) => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    },
-    (reason) => {
-      console.log('Got a response from the API');
-      return new Error();
-    },
-  );
+    .then(
+      (value) => {
+        console.log('Got a response from the API');
+        return { status: 200, body: 'success' };
+      },
+      (reason) => {
+        console.log('Got a response from the API');
+        return new Error();
+      },
+    );
 }
