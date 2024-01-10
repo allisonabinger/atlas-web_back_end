01b7a068e1437e43e8dcb154293c2bb957f6f02b
Allison <allisonabinger@gmail.com>
2024-01-10 12:19:56 -0600

    A - 01/10 12:19 PM - removed newPromise from reject functionality

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,3 +1,17 @@
 export default function getFullResponseFromAPI(success) {
-  //
+  // return a promise with boolean parameter
+  // if argument is true, resolve promise by passing object
+  // if argument is false, reject promise with error message
+  const newPromise = new Promise((resolve, reject) => {
+    if (success) {
+      const apiResponseMessage = {
+        status: 200,
+        body: 'Success',
+      };
+      resolve(apiResponseMessage);
+    } else {
+      reject('The fake API is not working currently');
+    }
+  });
+  return newPromise;
 }
