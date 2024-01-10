0119b59424c71ae11caa81cc648a627d006935c0
Allison <allisonabinger@gmail.com>
2024-01-10 12:58:04 -0600

    A - 01/10 12:57 PM - high-order function added to log

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -4,6 +4,9 @@ export default function handleResponseFromAPI(promise) {
   // when promise rejects, return an empty Error object
   // log response to console for every resolution
   const logAndReturn = (handler) => (...args) => {
+    // high-order, takes handler function and returns new function
+    // logs messages then does handler function
+    // i may not use this again because it was so tricky
     console.log('Got a response from the API');
     return handler(...args);
   }
