515c3f4c96f5b03353b505e0373169fc4177aeed
Allison <allisonabinger@gmail.com>
2024-01-11 10:12:56 -0600

    A - 01/11 10:12 AM - added array structure, does not pass lint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,13 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]);
+  return results.map((result, i) => ({
+    status: result.status,
+    value: result.value || result.reason,
+  }));
 }
