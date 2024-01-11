a2853264ac65dbd099192530ec84d2f74b7ae00b
Allison <allisonabinger@gmail.com>
2024-01-11 10:06:46 -0600

    A - 01/11 10:06 AM - main tested, lint untested

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ])
+    uploadPhoto(fileName),
+  ]);
 }
