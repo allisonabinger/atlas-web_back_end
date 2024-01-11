439c29ea9ffb82f6a0fc23d1c85c29e8c345dbf7
Allison <allisonabinger@gmail.com>
2024-01-11 11:26:34 -0600

    A - 01/11 11:26 AM - reverted back to storing promise as const, untested lint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,12 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const photoPromise = uploadPhoto(fileName);
   const userPromise = signUpUser(firstName, lastName);
-  return photoPromise + userPromise;
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, uploadPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason
+    })));
 }
