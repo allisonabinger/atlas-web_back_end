f1fa4c443834b30e8eb0228ec4931f788fccc7b7
Allison <allisonabinger@gmail.com>
2024-01-11 10:43:30 -0600

    A - 01/11 10:43 AM - changed method, not functional

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  
-  
-  const photoPromise = uploadPhoto(fileName) + signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  const userPromise = signUpUser(firstName, lastName);
+  return photoPromise + userPromise;
 }
