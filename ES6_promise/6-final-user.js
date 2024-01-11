24bb8c2c7ab33f59f11e0ddcb2c37884fdc6de41
Allison <allisonabinger@gmail.com>
2024-01-11 10:14:23 -0600

    A - 01/11 10:14 AM - lint passes

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,12 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]);
-  return results.map((result, i) => ({
+  return results.map((result) => ({
     status: result.status,
     value: result.value || result.reason,
   }));
