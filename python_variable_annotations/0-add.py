cfdeb59e7463a4d7f41584f0dde12dc4d878de86
Allison <allisonabinger@gmail.com>
2024-04-29 15:22:44 -0500

    A - 04/29 03:22 PM - Added spacing + documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# Type-annotated function that adds two floats and returns their sum as a float
+
 
 def add(a: float, b: float) -> float:
+    # returns float
     return a + b
