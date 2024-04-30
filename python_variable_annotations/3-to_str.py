00cc67b7534dc65efb9413edf91059f0d424be63
Allison <allisonabinger@gmail.com>
2024-04-30 10:45:19 -0500

    A - 04/30 10:45 AM - Task 3: converts to string, pycodestyle, documented

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,7 @@
-#!/
+#!/usr/bin/env python3
+""" Type-Annotated function: converts float n to string rep """
+
+
+def to_str(n: float) -> str:
+    """ returns the string rep. of the float n """
+    return str(n)
