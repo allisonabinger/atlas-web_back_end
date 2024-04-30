0061d8a0dc39522620d1db35740ed5ce77a46c1f
Allison <allisonabinger@gmail.com>
2024-04-30 10:40:42 -0500

    A - 04/30 10:40 AM - Task 2: floor, documented and pycodestyle

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Type Annotated function that returns floor of float n """
+import math
+
+
+def floor(n: float) -> int:
+    """ uses math.floor to return the floor of n """
+    return math.floor(n)
