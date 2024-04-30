0e72180d2a0ce2d8bf646702c365d7e2fa1a12d4
Allison <allisonabinger@gmail.com>
2024-04-30 11:00:40 -0500

    A - 04/30 11:00 AM - Attempted annotation in 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Type-Annotated function: takes list and returns sum as a float """
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ returns sum as a float """
     return sum(input_list)
