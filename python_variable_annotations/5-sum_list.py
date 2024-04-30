e58bb680595e7d35c406c5e0dd18a23d534cba80
Allison <allisonabinger@gmail.com>
2024-04-30 10:58:57 -0500

    A - 04/30 10:58 AM - Task 5: tested, documented, linter checker

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Type-Annotated function: takes list and returns sum as a float """
+
+
+def sum_list(input_list: list) -> float:
+    """ returns sum as a float """
+    return sum(input_list)
