bd513bc1d0bed7496ba5c7668e6aa9a28e441a8e
Allison <allisonabinger@gmail.com>
2024-05-06 14:44:34 -0500

    A - 05/06 02:44 PM - Task 0: Moved annotations above the import

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
+"""Understanding the basics of async with a corouting that waits"""
 import asyncio
 import random
-"""Understanding the basics of async with a corouting that waits"""
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """async coroutine that waits for a random delay before """
+    """async coroutine that waits for a random delay before"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
