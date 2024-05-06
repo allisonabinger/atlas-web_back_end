fc9a3bc891f113a0a965ad799f66f657ebe9f251
Allison <allisonabinger@gmail.com>
2024-05-06 15:49:56 -0500

    A - 05/06 03:49 PM - removed type annotation that was causing issues in task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 """Async Comprehensions and Generators: Task 0"""
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator():
     """Loops 10 times and generates a random num"""
     for _ in range(10):
         await asyncio.sleep(1)
