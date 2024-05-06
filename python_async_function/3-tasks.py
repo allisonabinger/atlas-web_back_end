13d20f57c25f5d5542e14624bed338d39f8f63d9
Allison <allisonabinger@gmail.com>
2024-05-06 15:35:36 -0500

    A - 05/06 03:35 PM - Task 3: Fixed pycode style

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -3,6 +3,7 @@
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """returns async task with non async func"""
     task = asyncio.get_event_loop()
---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""basics of async: Task 4"""
+from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """uses task_wait_random to duplicate outcomes of task 1"""
+    async_tasks = [task_wait_random(max_delay) for _ in range(n)]
+    =
+    return [await index for index in asyncio.as_completed(async_tasks)]
