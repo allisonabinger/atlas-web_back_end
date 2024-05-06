96015d244b53370d190330473bf82cd1c20da29c
Allison <allisonabinger@gmail.com>
2024-05-06 15:29:55 -0500

    A - 05/06 03:29 PM - Task 3: typed, annotated, tested!

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basics of Async: Task 3"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """returns async task with non async func"""
+    task = asyncio.get_event_loop()
+    return task.create_task(wait_random(max_delay))
