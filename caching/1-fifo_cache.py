35d62dfdd45b9dbf72cad833f40e7cf1ef46609c
Allison <allisonabinger@gmail.com>
2024-05-16 11:22:38 -0500

    A - 05/16 11:22 AM - Task 1 - FIFO: tested, pycodestyle

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -8,16 +8,22 @@ class FIFOCache(BaseCaching):
         """initiation"""
         super().__init__()
         self.queue = []
-    
+
     def put(self, key, item):
         """creates new key-item pair as long as input is valid"""
         if key is None or item is None:
             return
-        
+        """removes the first item in the queue if the queue is full"""
         if len(self.cache_data) >= self.MAX_ITEMS:
             discarded_key = self.queue.pop(0)
             del self.cache_data[discarded_key]
             print(f"DISCARD: {discarded_key}")
-        
+
         self.cache_data[key] = item
         self.queue.append(key)
+
+    def get(self, key):
+        """searchs for an item with a valid key input"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
