3c5fcdc31c29437b603f78dbc42ac7da1220635b
Allison <allisonabinger@gmail.com>
2024-05-16 12:17:38 -0500

    A - 05/16 12:17 PM - Finished README and CSV file set up, main files and gitignore prepared, and make file for task 0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Helper Function to take page and page_size and
+return a tuple of size two containing a start index and an end index
+corresponding to the range of indexes to return in a list for
+those particular pagination parameters.
+"""
