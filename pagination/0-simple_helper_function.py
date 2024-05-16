1f85e635cf9b6346ed7c444a503f88713007011d
Allison <allisonabinger@gmail.com>
2024-05-16 15:20:03 -0500

    A - 05/16 03:19 PM - Task 0: output matches prompt but structure seems incorrect.

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -7,7 +7,7 @@ def index_range(page: int, page_size: int) -> tuple:
     to the range of indices to return in a list for the pagination parameters
     given as page and page_size"""
 
-    start_index = (page - 1) * page_size
-    end_index = page * page_size - 1
+    start_index: int = (page - 1) * page_size
+    end_index: int = page * page_size
 
-    return start_index, end_index
+    return (start_index, end_index)
