f762642557bd76dabf4215a3413363f22122e659
Allison <allisonabinger@gmail.com>
2024-05-16 09:38:41 -0500

    A - 05/16 09:38 AM - Base Caching Task 0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,3 +1,31 @@
 #!/usr/bin/python3
 """ Task 0: Basics of Caching """
-BaseCaching = 
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic Caching class inherited from BaseCaching with simple
+    put and get methods"""
+
+    def put(self, key, item):
+        """creates new key-item pair as long as input is valid"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """searches for item based on key as long as key is valid"""
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
+
+
+# Example usage:
+    # cache = BasicCache()
+    # cache.put('a', 1)
+    # cache.put('b', 2)
+    #
+    # print(cache.get('a')) -> Output: 1
+    # print(cache.get('c')) -> Output: None
+    #
--- caching/base_caching.py
+++ caching/base_caching.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" BaseCaching module
-"""
+""" BaseCaching module"""
+
 
 class BaseCaching():
     """ BaseCaching defines:
