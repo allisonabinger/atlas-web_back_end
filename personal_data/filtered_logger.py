cc6d98ec4c71d9fd56f61647f1fad5ae9905cc54
Allison <allisonabinger@gmail.com>
2024-05-20 16:12:12 -0500

    A - 05/20 04:11 PM - Task 1: updated filtered logger to implement RedactingFormatter class, output is a bit different, sending to checker

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -27,3 +28,23 @@ def filter_datum(
 
     # [^;]+ matches one or more chars that are not ; to capture the value of
     #  the following 'field= or until the next ; or end of string
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[ATLAS] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """initialization, accepts list of strings as fields"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formats the specific log records"""
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)
