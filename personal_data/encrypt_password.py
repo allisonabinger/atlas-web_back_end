54fabd401cdf442d6e920944f2a817c5677a8191
Allison <allisonabinger@gmail.com>
2024-05-21 13:09:05 -0500

    A - 05/21 01:08 PM - added validation method to encrypt_password, docced, type annotated, and linter checked

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validates if the provided password matches the hashed_password
+    using bcrypt's checkpw"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
