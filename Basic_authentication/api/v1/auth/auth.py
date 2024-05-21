6638c99b406c270f54cd66751fd608c79cfc2e1c
Allison <allisonabinger@gmail.com>
2024-05-21 15:57:16 -0500

    A - 05/21 03:57 PM - Added functionality to require_auth in auth.py

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,9 +11,30 @@ class Auth():
     """base class for authentication system - template for later"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns false
-        path and excluded_paths will be used later"""
-        return False
+        """returns false if authentication is not required for given path
+        i.e. if path is in excluded_paths.
+
+        returns true if path requires authentication
+        i.e. if path is NOT in excluded_paths"""
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        """normalize path by ensuring it ends with /"""
+        normalized_path = path if path.endswith('/') else path + '/'
+
+        """check if normalized path is in the excluded paths"""
+        for ex_path in excluded_paths:
+            if ex_path.endswith('/'):
+                normalized_excluded_path = ex_path
+            else:
+                normalized_excluded_path = ex_path + '/'
+
+            if normalized_path == normalized_excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None
