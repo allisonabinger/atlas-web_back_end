23a7af23221cfb15f713a9be685772a7b2175fb3
Allison <allisonabinger@gmail.com>
2024-05-22 09:47:00 -0500

    A - 05/22 09:46 AM - Implemented email and pass extraction in basic_auth, all .py files in the repository pass linter now

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,3 +43,16 @@ class BasicAuth(Auth):
             return decoded_string
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """returns the user email and password from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None
+        if ':' not in decoded_base64_authorization_header:
+            return None
+
+        user_email, user_pass = decoded_base64_authorization_header.split(':', 1)  # noqa
+        return user_email, user_pass
