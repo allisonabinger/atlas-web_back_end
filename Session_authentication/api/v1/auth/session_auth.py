e77481dcb1a78309d806dcaedb54c8cb5cf3e534
Allison <allisonabinger@gmail.com>
2024-05-23 11:42:37 -0500

    A - 05/23 11:42 AM - Added session_id to session_auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -5,7 +5,20 @@ from typing import TypeVar
 from api.v1.auth.auth import Auth
 from models.user import User
 import base64
+import uuid
 
 
 class SessionAuth(Auth):
     """Class is empty: first step for creating new authentication mechanism"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id = user_id
+
+        return session_id
