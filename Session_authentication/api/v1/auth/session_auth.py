767337fd973c8e8c8c29868074c8131b2d397a6a
Allison <allisonabinger@gmail.com>
2024-05-23 11:59:18 -0500

    A - 05/23 11:59 AM - Added user_id_for_session_id method to session_auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -9,7 +9,7 @@ import uuid
 
 
 class SessionAuth(Auth):
-    """Class is empty: first step for creating new authentication mechanism"""
+    """Session Authentication Class"""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
@@ -23,3 +23,12 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns the user id based on a session ID"""
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
