61d08e18fb208ab6979a234d80256d69ee68b9cf
Allison <allisonabinger@gmail.com>
2024-05-23 11:53:02 -0500

    A - 05/23 11:52 AM - Updated the dictionary value in create_session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -20,6 +20,6 @@ class SessionAuth(Auth):
             return None
 
         session_id = str(uuid.uuid4())
-        self.user_id_by_session_id = user_id
+        self.user_id_by_session_id[session_id] = user_id
 
         return session_id
