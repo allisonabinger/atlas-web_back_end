ad57786f6b5b710357d063fa978d21efd43c8e62
Allison <allisonabinger@gmail.com>
2024-05-29 16:37:29 -0500

    A - 05/29 04:37 PM - Added inception classes to TestMemoize and tested properly

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from typing import Dict, Any, Tuple
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -58,3 +58,27 @@ class TestGetJson(unittest.TestCase):
             mocked_get.assert_called_once_with(test_url)
             # makes sure the result was called and is the same as test_payload
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test Suite for memoize method"""
+
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_instance = TestClass()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:  # noqa
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            mock_method.assert_called_once()
