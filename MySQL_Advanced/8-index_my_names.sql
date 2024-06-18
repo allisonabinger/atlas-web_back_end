f7b43ae0eea9854265a4c8fa47ea817634de715a
Allison <allisonabinger@gmail.com>
2024-06-18 16:40:10 -0500

    A - 06/18 04:40 PM - added link to names.sql to task 8

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,7 @@
 -- holberton database must be created and prepared before this script can run.
 -- uses names.sql
+-- no longer in directory, but use this link while logged into the intranet to download.
+-- https://intranet-projects-files.s3.amazonaws.com/holbertonschool-webstack/632/names.sql.zip
 
 -- Task 8: creates an index 'idx_name_first' on the table 'names' and the first letter of 'name'
 CREATE INDEX idx_name_first ON names (name(1));
