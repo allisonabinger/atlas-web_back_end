2b001d0dbabbfd3a6c2ef8cd4283fbc610ddfada
Allison <allisonabinger@gmail.com>
2024-06-18 13:43:59 -0500

    A - 06/18 01:43 PM - Created 2-fans.sql // tested but does not return exact # of fans as intranet example

---
+++ MySQL_Advanced/2-fans.sql
@@ -0,0 +1,17 @@
+-- holberton database must be created and prepared before this script can run.
+
+-- Task 2: Ranks country origins of bands, ordered by the number of (non-unique) fans
+-- references metal_bands.sql located in directory
+-- column names are origin and nb_fans
+
+-- Tip! Uses a temporary table to only exist for the duration of the session in which theyre created.
+-- Improves performance since temp. tables are typically faster than recalculating the same result.
+-- This script will not store this table as a permanent object, but will select from the temporary table.
+
+CREATE TEMPORARY TABLE IF NOT EXISTS band_fans AS (
+    SELECT origin, COUNT(fans) AS nb_fans FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC
+);
+
+SELECT origin, nb_fans FROM band_fans;
