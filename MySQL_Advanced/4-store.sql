8f42ab8f8d69ec52cae299f6338551e88367d6e2
Allison <allisonabinger@gmail.com>
2024-06-18 15:04:16 -0500

    A - 06/18 03:04 PM - Added set up for task 4

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- holberton database must be created and prepared before this script can run.
+-- uses 4-main.sql and 4-init.sql
+
+-- Task 4: Creates a trigger that decreases the quantity of an item after adding a new order
+-- Quantity in the table 'items' can be negative
+
+
