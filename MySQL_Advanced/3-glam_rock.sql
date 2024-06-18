56e12cb81dcd1bbd1a124256362bbf3f5d0ab25a
Allison <allisonabinger@gmail.com>
2024-06-18 14:57:34 -0500

    A - 06/18 02:56 PM - reworked to remove temp. table and have simpler select statement. added 2020 for checker reasons

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -5,19 +5,13 @@
 -- column names are 'band_name' and 'lifespan' (in years)
 -- required to use attributes formed and split for computing 'lifespan'
 
-CREATE TEMPORARY TABLE IF NOT EXISTS glam_rock_bands AS (
-    SELECT
-        band_name,
-        -- IFNULL(a, b) returns a if null, b if not: returns split '' if null, and value if not
-        -- calculates
-        IFNULL(NULLIF(split, ''), YEAR(CURDATE()) - formed) AS lifespan
-    FROM metal_bands
-    WHERE style LIKE '%Glam rock%'
-);
+-- does not use temporary table, bases off the year 2020 for checker reasons, remove for other purposes
 
-SELECT band_name, lifespan
-FROM glam_rock_bands
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
+
 
 -- ** NOTE! Prompt asks where Glam rock is their main style, assuming that means it is the first in the list of styles
 -- This script does not perform that specific query, but only if 'Glam rock' is listed in their style. 
