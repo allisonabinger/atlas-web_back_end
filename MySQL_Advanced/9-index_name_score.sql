5a86e327dbc06dadd01d352dd0753145e65c2dd6
Allison <allisonabinger@gmail.com>
2024-06-18 16:39:47 -0500

    A - 06/18 04:39 PM - Removed names.sql and added link to task 9

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,5 +1,7 @@
 -- holberton database must be created and prepared before this script can run.
--- uses names.sql
+-- uses names.sql 
+-- no longer in directory, but use this link while logged into the intranet to download.
+-- https://intranet-projects-files.s3.amazonaws.com/holbertonschool-webstack/632/names.sql.zip
 
 -- Task 9: creates an index idx_name_first_score on the table names,
 -- and the first letter of name and the score
