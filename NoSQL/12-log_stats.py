c2ed7bb092d74d4f81d983d24975d4c43a660df7
Allison <allisonabinger@gmail.com>
2024-06-19 14:53:34 -0500

    A - 06/19 02:53 PM - Added more documentation

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,25 +2,33 @@
 """Task 12: provides stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
+
+"""Delcare Vars for use"""
 HOST = 'localhost'
 PORT = 27017
 DATABASE_NAME = 'logs'
 COLLECTION_NAME = 'nginx'
 
+"""Establish connection and gather collection"""
 client = MongoClient(HOST, PORT)
 db = client[DATABASE_NAME]
 collection = db[COLLECTION_NAME]
 
+"""gather logs"""
 total_logs = collection.count_documents({})
 print(f"{total_logs} logs")
 
+"""count methods"""
 http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 method_counts = {method: collection.count_documents({"method": method}) for method in http_methods}  # noqa
 
+"""print method counts"""
 print("Methods:")
 for method, count in method_counts.items():
     print(f"\tmethod {method}: {count}")
 
+"""gather status check counts"""
 status_check = collection.count_documents({"method": "GET", "path": "/status"})  # noqa
 
+"""print number of status checks"""
 print(f"{status_check} status check")
