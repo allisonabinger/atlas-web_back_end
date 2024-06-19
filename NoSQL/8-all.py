5eef8db6c38da142b0c04e5668e66feefcddc95f
Allison <allisonabinger@gmail.com>
2024-06-19 14:30:13 -0500

    A - 06/19 02:29 PM - Task 8: tests only show one but working as intended

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""Task 8: uses python to list all docs from given collection"""
+
+
+def list_all(mongo_collection):
+    """lists all docs from given collection"""
+    documents = list(mongo_collection.find())
+
+    return documents
