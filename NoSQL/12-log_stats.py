2b4d95f901b210b706104fbfc6c346e3c3eaa4e9
Allison <allisonabinger@gmail.com>
2024-06-19 14:55:09 -0500

    A - 06/19 02:54 PM - Wrapped code into a function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,33 +2,36 @@
 """Task 12: provides stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
+def nginx_log_stats():
+    """Delcare Vars for use"""
+    HOST = 'localhost'
+    PORT = 27017
+    DATABASE_NAME = 'logs'
+    COLLECTION_NAME = 'nginx'
 
-"""Delcare Vars for use"""
-HOST = 'localhost'
-PORT = 27017
-DATABASE_NAME = 'logs'
-COLLECTION_NAME = 'nginx'
+    """Establish connection and gather collection"""
+    client = MongoClient(HOST, PORT)
+    db = client[DATABASE_NAME]
+    collection = db[COLLECTION_NAME]
 
-"""Establish connection and gather collection"""
-client = MongoClient(HOST, PORT)
-db = client[DATABASE_NAME]
-collection = db[COLLECTION_NAME]
+    """gather logs"""
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
 
-"""gather logs"""
-total_logs = collection.count_documents({})
-print(f"{total_logs} logs")
+    """count methods"""
+    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: collection.count_documents({"method": method}) for method in http_methods}  # noqa
 
-"""count methods"""
-http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {method: collection.count_documents({"method": method}) for method in http_methods}  # noqa
+    """print method counts"""
+    print("Methods:")
+    for method, count in method_counts.items():
+        print(f"\tmethod {method}: {count}")
 
-"""print method counts"""
-print("Methods:")
-for method, count in method_counts.items():
-    print(f"\tmethod {method}: {count}")
+    """gather status check counts"""
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})  # noqa
 
-"""gather status check counts"""
-status_check = collection.count_documents({"method": "GET", "path": "/status"})  # noqa
+    """print number of status checks"""
+    print(f"{status_check} status check")
 
-"""print number of status checks"""
-print(f"{status_check} status check")
+if __name__ == "__main__":
+    nginx_log_stats()
