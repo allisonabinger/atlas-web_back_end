17c9bb0ba70810573dabc1f57b12cc35e397dd7f
Allison <allisonabinger@gmail.com>
2024-06-20 14:45:10 -0500

    A - 06/20 02:45 PM - Added usage comments

--- Node_JS/mains/3-main_0.js
+++ Node_JS/mains/3-main_0.js
@@ -7,3 +7,13 @@ countStudents("nope.csv")
         .catch((error) => {
         console.log(error);
     });
+
+// Test in terminal:
+//
+// node 3-main_0.js or node mains/3-main_0.js
+//
+// Expected Output:
+//
+//
+// Error: Cannot load the database or access database file 
+//      at countStudents (/Users/allisonbinger/Desktop/Atlas/T4/atlas-web_back_end/Node_JS/3-read_file_async.js:33:15)
--- Node_JS/mains/3-main_1.js
+++ Node_JS/mains/3-main_1.js
@@ -8,3 +8,16 @@ countStudents("../database.csv")
         console.log(error);
     });
 console.log("After!");
+
+// Test in terminal:
+//
+// node 3-main_0.js or node mains/3-main_0.js
+//
+// Expected Output:
+//
+//
+// After!
+// Number of students: 10
+// Number of students in CS: 6. List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie
+// Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy
+// Done!
