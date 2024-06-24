829d5347af2f3d2b56b9ca4d54eb4a10e7eb3efa
Allison <allisonabinger@gmail.com>
2024-06-24 14:32:56 -0500

    A - 06/24 02:32 PM - Used express for task 6 to spin up local server, added logtatements! runs correctly!

---
+++ Node_JS/6-http_express.js
@@ -0,0 +1,17 @@
+// Task 6: Create a small HTTP server using the Express module
+// listens on port 1245
+// displays plaintext msg in page body for endpoint '/'
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('Endpoint "/" accessed.');
+    res.send('Hello Holberton School!');
+})
+
+app.listen(1245, () => {
+    console.log('server is running on port 1245.')
+})
+
+module.exports = app;
