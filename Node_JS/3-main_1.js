b43d9819ff138f9de0483ab2f7a4123545197a25
Allison <allisonabinger@gmail.com>
2024-06-26 17:08:49 -0500

    A - 06/26 05:08 PM - Adjusted main file for task 3 due to nature of 3-read_file_async

--- Node_JS/3-main_1.js
+++ Node_JS/3-main_1.js
@@ -1,14 +1,22 @@
+// NOTE: this main file was adjusted due to other files needing 3-read_file_async
+// to return and not just log the values. 
+
 const countStudents = require('./3-read_file_async');
 
 countStudents('./database.csv')
-    .then(() => {
+    .then(({ total, counts, lists }) => {
+        console.log("After!");
+        console.log(`Number of students: ${total}`);
+        for (const field in counts) {
+            if (counts.hasOwnProperty(field)) {
+                console.log(`Number of students in ${field}: ${counts[field]}. List: ${lists[field].join(', ')}`);
+            }
+        }
         console.log("Done!");
     })
-        .catch((error) => {
-        console.log(error);
+    .catch((error) => {
+        console.error(error);
     });
-console.log("After!");
-
 // Test in terminal:
 //
 // node 3-main_1.js or node mains/3-main_1.js
