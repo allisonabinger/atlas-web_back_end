ee0c37dd04208e1b791fb6d408de19c28fda309f
Allison <allisonabinger@gmail.com>
2024-06-27 15:10:41 -0500

    A - 06/27 03:10 PM - finished tak 9 server and adjusted tests, headers and now accurate

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -16,15 +16,52 @@ describe('Index page', () => {
             });
     });
 
-    if('should have content-type text-html and utf-8 encoding', (done) => {
+    it('should have correct header attributes', (done) => {
         chai.request('http://localhost:7865')
             .get('/')
             .end((err, res) => {
-                expect(res).to.have.header('content-type', 'text-html; charset=utf-8');
-                done()
+                expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
+                done();
+            });
+    });
+});
+
+describe('Cart page', () => {
+    it('should return status code 200 and correct message when :id is a number', (done) => {
+        chai.request('http://localhost:7865')
+            .get('/cart/23')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.text).to.equal('Payment methods for cart 23');
+                done();
             });
     });
 
-    
+    it('should have correct header attributes when :id is a number', (done) => {
+        chai.request('http://localhost:7865')
+            .get('/cart/23')
+            .end((err, res) => {
+                expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
+                done();
+            });
+    });
 
-});
+    it('should return status 404 and correct message :id is not a number', (done) => {
+        chai.request('http://localhost:7865')
+            .get('/cart/applesauce')
+            .end((err, res) => {
+                expect(res).to.have.status(404);
+                expect(res.text).to.equal('Id must be a number');
+                done();
+            });
+    });
+
+    it('should return correct header attributes :id is not a number', (done) => {
+        chai.request('http://localhost:7865')
+            .get('/cart/applesauce')
+            .end((err, res) => {
+                expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
+                done();
+            });
+    });
+})
