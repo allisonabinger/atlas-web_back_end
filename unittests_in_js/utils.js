4cce1c5acdedd6893f2eaa284eca0edebb4f5905
Allison <allisonabinger@gmail.com>
2024-06-27 13:30:37 -0500

    A - 06/27 01:30 PM - created the utils module

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,24 @@
+// utils module
+const Utils = {
+    calculateNumber: (type, a, b) => {
+        const roundedA = Math.round(a);
+        const roundedB = Math.round(b);
+    
+        switch (type) {
+            case 'SUM':
+                return roundedA + roundedB;
+            case 'SUBTRACT':
+                console.log(``)
+                return roundedA - roundedB;
+            case 'DIVIDE':
+                if (roundedB === 0) {
+                    return 'Error';
+                }
+                return roundedA / roundedB;
+            default:
+                throw new Error('Invalid type: must be SUM, SUBTRACT, or DIVIDE');
+        }
+    }
+}
+
+module.exports = Utils;
