2b288eec3e569c8f1c7e52b3b9b9e2e25140831e
Allison <allisonabinger@gmail.com>
2024-06-27 14:27:24 -0500

    A - 06/27 02:27 PM - Added api folder, copied package.json, basic express server started

---
+++ unittests_in_js/8-api/api.js
@@ -0,0 +1,12 @@
+// task 8
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('Endpoint "/" accessed.');
+    res.status(400).send('Welcome to the payment system');
+})
+
+app.listen(7865, () => {
+    console.log('API available on localhost port 7865')
+})
