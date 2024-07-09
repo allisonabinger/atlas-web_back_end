103001d15863f0e78b23b247aa105b9f88995956
Allison <allisonabinger@gmail.com>
2024-07-09 12:46:59 -0500

    A - 07/09 12:46 PM - implemented job creator, added error handler for queue, plus listeners for enqueue, complete, and failed for job processes

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,33 @@
+import kue from 'kue';
+import { v4 as uuidv4 } from 'uuid';
+
+const queue = kue.createQueue();
+const jobData = {
+    phoneNumber: '+1234567890',
+    message: 'Hello from Kue!'
+}
+
+const job = queue.create('push_notification_code', jobData);
+
+// enqueue is fired when the job is successfully enqueued in the queue
+job.on('enqueue', () => {
+    console.log(`Notification job create: ${job.id}`);
+});
+
+// complete is fired when job is successfully processed
+job.on('complete', () => {
+    console.log('Notification job completed');
+});
+
+// failed is fired when job does not process successfully
+job.on('failed', () => {
+    console.log('Notification job failed');
+});
+
+job.save();
+
+
+// adding queue error event listener
+queue.on('error', (err) => {
+    console.error('Queue error:', err);
+});
