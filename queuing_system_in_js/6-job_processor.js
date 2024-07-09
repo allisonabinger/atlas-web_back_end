731b85cf4cb00232f37a2ae0093d632f22fb8643
Allison <allisonabinger@gmail.com>
2024-07-09 12:57:33 -0500

    A - 07/09 12:57 PM - Job processor fully functional, fixed typo in queue name

--- queuing_system_in_js/6-job_processor.js
+++ queuing_system_in_js/6-job_processor.js
@@ -6,7 +6,7 @@ function sendNotification(phoneNumber, message) {
 }
 
 // processes jobs from the queue
-queue.process('push_notification_node', (job, done) => {
+queue.process('push_notification_code', (job, done) => {
     const { phoneNumber, message } = job.data;
     sendNotification(phoneNumber, message);
     done();
